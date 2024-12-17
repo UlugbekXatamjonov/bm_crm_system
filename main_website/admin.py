fc435eeb190245da4690a335143624d18743b20d
UlugbekXatamjonov <xatamjonovulugbek17@gmail.com>
2024-12-17 11:42:13 +0500

    -Bitiruvchilar uchun Graduation_year va Graduate modellari qo'shildi

--- main_website/admin.py
+++ main_website/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Announcement, Parents_opinion, Contact_us
+from .models import Announcement, Parents_opinion, Contact_us,  Graduation_year, Graduate
 
 # Register your models here.
 
@@ -22,9 +22,16 @@ class Contect_us_Admin(admin.ModelAdmin):
     list_display = ['name', 'phone', 'status', 'created_on']
     list_filter = ['status', 'created_on']
 
+@admin.register(Graduation_year)
+class Graduation_year_Admin(admin.ModelAdmin):
+    list_display = ['year','number_of_graduates','number_of_enrollees','status']
+    list_filter = ['status', 'created_on']
 
 
-
+@admin.register(Graduate)
+class Graduate_Admin(admin.ModelAdmin):
+    list_display = ['name', 'graduation_year', 'university', 'status']
+    list_filter = ['status', 'graduation_year', 'created_on']    
 
 
 
--- main_website/models.py
+++ main_website/models.py
@@ -74,5 +74,46 @@ class Contact_us(models.Model):
         return self.name
     
     
+class Graduation_year(models.Model):
+    """ Bitiruvchilar bitirgan yili """
     
+    year = models.PositiveIntegerField(verbose_name="Yil nomi")
+    number_of_graduates = models.PositiveIntegerField(verbose_name="Bitiruvchilar soni")
+    number_of_enrollees = models.PositiveIntegerField(verbose_name="O'qishga kirganlar soni")
+    
+    status = models.BooleanField(default=True, verbose_name="Holati")
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = "Bitiruv yili"
+        verbose_name_plural = "Bitiruv yillari"
+    
+    def __str__(self):
+        return f"{self.year}"
+    
+    
+class Graduate(models.Model):
+    """ Bitiruvchilar uchun model """
+    
+    name = models.CharField(max_length=250, verbose_name="Ismi")
+    graduation_year = models.ForeignKey(Graduation_year, on_delete=models.CASCADE, related_name="graduates", verbose_name="Yil")
+    university = models.CharField(max_length=250, verbose_name="Universitet nomi")
+    photo = models.ImageField(upload_to="graduates_photo/%Y/", verbose_name="Rasm", null=True, blank=True)
+    
+    status = models.BooleanField(default=True, verbose_name="Holati")
+    created_on = models.DateTimeField(auto_now_add=True)
 
+
+    class Meta:
+        verbose_name = "Bitiruvchi"
+        verbose_name_plural = "Bitiruvchilar"
+        
+    def __str__(self):
+        return self.name
+    
+    
+    
+    
+    
+    
+    
